c8e3f11b90871dd0d37866dc3a895bdd5d2f4f71
NIPE-SYSTEMS <dev.nipe.systems@gmail.com>
2018-07-28 14:22:12 +0200

    Improve API

--- process.go
+++ process.go
@@ -3,116 +3,118 @@ package goprocess
 import (
 	"bufio"
 	"errors"
-	"io"
 	"os"
 	"os/exec"
 	"syscall"
+	"io"
 )
 
-// close(messagesIn) kills the process and therefore ends it
-// when the process ends (by itself or by a kill) it closes the messageOut-channel
-func NewProcess(args []string, stdin chan []byte, signals chan os.Signal) (chan []byte, chan []byte, error) {
+// Events that can occur and how the process object reacts to them:
+// - creation error: return error
+// - Start() error: return error
+// - stdin gets closed: close stdin on process
+// - signals gets closed: do nothing (consequence: killing is not possible anymore)
+// - process closes stdout pipe: close stdout channel
+// - process closes stderr pipe: close stderr channel
+// - process terminates: do nothing (pipes are closed automatically)
+
+// NewProcess creates a new process in the background and provides a simple interface for standard I/O. It consumes and produces []byte messages that are received or will be sent to the process. The exchanged messages are split at newlines (so messages on the stdin-channel should not contain any newlines).
+//
+// Closing the stdin-channel will close the corresponding pipe to the process. When the process closes the stdout or stderr pipes the corresponding channels will be closed. Closing the signals-channel does nothing but makes it impossible to interact with the process via signals afterwards.
+//
+// To ensure that all goroutines are stopped, send a terminating signal over the signals-channel (e.g. SIGINT, SIGTERM) and close both the stdin- and signals channel.
+//
+// This interface does not allow to check explicitly whether the process actually exited. Nevertheless it is possible to check the closed-state of the output-channels (stdout, stderr).
+//
+// The following code creates a new process with all required inputs:
+//
+//     stdin := make(chan []byte)
+//     signals := make(chan os.Signal)
+//     stdout, stdin, err := NewProcess([]string{"cat"}, stdin, signals)
+//     if err != nil {
+//         // handle error
+//     }
+//
+// The following code stops the process by sending a SIGINT to the process:
+//
+//     signals <- syscall.SIGINT
+//     close(stdin)
+//     close(signals)
+//
+// It is also possible to forward signals from the parent process to the created process:
+//
+//     signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+func NewProcess(args []string, stdin <-chan []byte, signals <-chan os.Signal) (<-chan []byte, <-chan []byte, error) {
 	if len(args) <= 0 {
 		return nil, nil, errors.New("no arguments specified")
 	}
 	command := exec.Command(args[0], args[1:]...)
-	stdinWriter, stdoutScanner, stderrScanner, err := getReaders(command)
+	// https://medium.com/@felixge/killing-a-child-process-and-all-of-its-children-in-go-54079af94773
+	command.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	stdinWriter, err := command.StdinPipe()
 	if err != nil {
 		return nil, nil, err
 	}
-	// https://medium.com/@felixge/killing-a-child-process-and-all-of-its-children-in-go-54079af94773
-	command.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	// doneStdin informs about the closed messagesIn-channel
-	doneStdin := sendStdin(stdinWriter, stdin)
-	// doneStdout/doneStderr informs about the closed stdout-/stderr-pipe from the process (usually triggers with doneProcess)
-	stdout, doneStdout := recvStdout(stdoutScanner)
-	stderr, doneStderr := recvStderr(stderrScanner)
-	// doneProcess informs about the terminated process (usually triggers with doneStdout)
-	doneProcess := runProcess(command)
+	stdoutPipe, err := command.StdoutPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	stdoutScanner := bufio.NewScanner(stdoutPipe)
+	stderrPipe, err := command.StderrPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	stderrScanner := bufio.NewScanner(stderrPipe)
+	err = command.Start()
+	if err != nil {
+		return nil, nil, err
+	}
+	sendStdin(stdinWriter, stdin)
+	stdout := recvStdout(stdoutScanner)
+	stderr := recvStderr(stderrScanner)
 	forwardSignals(command, signals)
 	go func() {
-		select {
-		case <-doneStdin:
-			// TODO: ignore stdin closing
-			// https://medium.com/@felixge/killing-a-child-process-and-all-of-its-children-in-go-54079af94773
-			syscall.Kill(-command.Process.Pid, syscall.SIGINT)
-		case <-doneStdout:
-		case <-doneStderr:
-		case <-doneProcess:
-		}
+		command.Wait()
 	}()
 	return stdout, stderr, nil
 }
 
-func getReaders(command *exec.Cmd) (io.WriteCloser, *bufio.Scanner, *bufio.Scanner, error) {
-	stdin, err := command.StdinPipe()
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	stdout, err := command.StdoutPipe()
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	stderr, err := command.StderrPipe()
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	return stdin, bufio.NewScanner(stdout), bufio.NewScanner(stderr), nil
-}
-
-func sendStdin(stdinWriter io.WriteCloser, stdin chan []byte) chan struct{} {
-	done := make(chan struct{}, 1) // will not block since done has capacity = 1
+func sendStdin(stdinWriter io.WriteCloser, stdin <-chan []byte) {
 	go func() {
 		for msg := range stdin {
 			_, err := stdinWriter.Write(append(msg, "\n"...))
 			if err != nil {
-				//log.Println("failed to write msg:", err)
 				// ignore write error
 				continue
 			}
-			// TODO: flush
 		}
-		done <- struct{}{}
+		stdinWriter.Close()
 	}()
-	return done
 }
 
-func recvStdout(stdoutScanner *bufio.Scanner) (chan []byte, chan struct{}) {
+func recvStdout(stdoutScanner *bufio.Scanner) <-chan []byte {
 	stdout := make(chan []byte, 1024)
-	done := make(chan struct{}, 1) // will not block since done has capacity = 1
 	go func() {
 		for stdoutScanner.Scan() {
 			stdout <- stdoutScanner.Bytes()
 		}
 		close(stdout)
-		done <- struct{}{}
 	}()
-	return stdout, done
+	return stdout
 }
 
-func recvStderr(stderrScanner *bufio.Scanner) (chan []byte, chan struct{}) {
+func recvStderr(stderrScanner *bufio.Scanner) <-chan []byte {
 	stderr := make(chan []byte, 1024)
-	done := make(chan struct{}, 1) // will not block since done has capacity = 1
 	go func() {
 		for stderrScanner.Scan() {
 			stderr <- stderrScanner.Bytes()
 		}
 		close(stderr)
-		done <- struct{}{}
 	}()
-	return stderr, done
+	return stderr
 }
 
-func runProcess(command *exec.Cmd) chan struct{} {
-	done := make(chan struct{}, 1) // will not block since done has capacity = 1
-	go func() {
-		command.Run()
-		done <- struct{}{}
-	}()
-	return done
-}
-
-func forwardSignals(command *exec.Cmd, signals chan os.Signal) {
+func forwardSignals(command *exec.Cmd, signals <-chan os.Signal) {
 	go func() {
 		for s := range signals {
 			// https://medium.com/@felixge/killing-a-child-process-and-all-of-its-children-in-go-54079af94773
